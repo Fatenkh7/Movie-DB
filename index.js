b816c4a3a92eb3aff5269d1cbade6749e1d96444
Faten_khoder <Khoderfaten5@gmail.com>
2022-12-26 15:00:32 +0000

    step 3

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("ok");
 });
-
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "ok" });
+});
+app.get("/time", (req, res) => {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  res.status(200).json({ message: `${hours}: ${minutes}` });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
