96a13a4cfc2bd06e2b52f6bbc99bbd0611ff2329
Faten_khoder <Khoderfaten5@gmail.com>
2022-12-26 22:54:48 +0000

    step 4

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+];
 
 app.get("/", (req, res) => {
   res.send("ok");
@@ -38,6 +44,33 @@ app.get("/search", (req, res) => {
     res.send({ status: 200, message: "ok", data: req.query.s });
   }
 });
+
+//movies
+// function list() {
+//   movies.map((element) => {
+//     console.log(element.title);
+//   });
+// }
+//create movies
+app.get("/movies/create", (req, res) => {
+  res.status(200).json({ message: "done" });
+});
+
+//edit movies
+app.get("/movies/edit", (req, res) => {
+  res.status(200).json({ message: "done" });
+});
+
+//delete movies
+app.get("/movies/delete", (req, res) => {
+  res.status(200).json({ message: "done" });
+});
+
+//read movies
+app.get("/movies/read", (req, res) => {
+  res.status(200).json({ data: movies });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
